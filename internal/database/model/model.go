d803117e3c9d47f2ea629fca044af9e2dcc8fb75
gen.liu <gen.liu@fliqt.io>
2025-02-22 20:17:47 +0800

    define data model

--- internal/database/model/model.go
+++ internal/database/model/model.go
@@ -1,1 +1,11 @@
 package model
+
+import "time"
+
+type MemeCoin struct {
+	ID              uint      `gorm:"primaryKey;autoIncrement" json:"id"`
+	Name            string    `gorm:"unique;not null" json:"name"`
+	Description     string    `json:"description"`
+	CreatedAt       time.Time `json:"created_at"`
+	PopularityScore int       `json:"popularity_score"`
+}
