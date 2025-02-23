59f36cf1ca109920db187a62b2c31e8b33041520
gen.liu <gen.liu@fliqt.io>
2025-02-23 16:26:05 +0800

    implement test for health service

---
+++ internal/service/health_service_test.go
@@ -0,0 +1,19 @@
+package service
+
+import (
+	"context"
+	"testing"
+
+	"github.com/nlsh710599/still-practice/internal/result"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHealth(t *testing.T) {
+	t.Run("Test Health", func(t *testing.T) {
+		healthService := &HealthServiceImpl{}
+		healthResult, err := healthService.Health(context.Background())
+
+		assert.Nil(t, err)
+		assert.Equal(t, result.HealthResult{Status: true}, healthResult)
+	})
+}
